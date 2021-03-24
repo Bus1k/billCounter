2d52af7e6b5e51b28b6425b93d75fb364534f9b8
grzegorz.bu <grzegorz.bu@modulesgarden.com>
2021-03-24 21:44:58 +0100

    add bill migration model and controller

---
+++ app/Models/Bill.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bill extends Model
+{
+    use HasFactory;
+
+    public $timestamps = true;
+
+    protected $guarded = [
+        'id'
+    ];
+
+    protected $fillable = [
+        'user_id',
+        'description',
+        'type',
+        'amount',
+        'photo_name',
+        'photo_location',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2021_03_24_203330_create_bills_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('description');
+            $table->string('type');
+            $table->decimal('amount', 65, 2);
+            $table->string('photo_name')->nullable();
+            $table->string('photo_location')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bills');
+    }
+}
